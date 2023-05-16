808965573aabbeaffb1e879fab0ca0a64d73e8fa
ommanirai <ommanirai27@gmail.com>
2023-05-16 10:56:51 +0545

    array

---
+++ array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int x[5];
+    
+    /*
+    x[0] = 101
+    x[1] = 10
+    int => data type
+    x => array name
+    5 => array length
+    */
+    //    printf("%d", x[0]);
+    printf("enter the numbers");
+    for (int i = 0; i < 5; i++)
+    {
+        // printf("%d\n", x[i]);
+        scanf("%d", &x[i]);
+    }
+
+    for(int i = 0; i<5; i++){
+        printf("The number you entered is: %d\n", x[i]);
+    }
+    // printf("The number you entered is: %d\n", x[0]);
+    // printf("The number you entered is: %d\n", x[1]);
+    // printf("The number you entered is: %d\n", x[2]);
+    // printf("The number you entered is: %d\n", x[3]);
+    // printf("The number you entered is: %d\n", x[4]);
+
+    return 0;
+}
--- fibonacci.c
+++ fibonacci.c
@@ -1,19 +1,32 @@
-#include<stdio.h>
-
-int main(){
+#include <stdio.h>
+int main()
+{
     int a = 0;
     int b = 1;
     int c, n;
-    printf("enter the number to find fibonacci series");
+    printf("enter the number to find fibonacci series\n");
     scanf("%d", &n);
-    printf("%d\n", a);
-    printf("%d\n", b);
-    for(int i=0;i<n; i++){
-        c = a + b; // 0, 1, 1 , 2, 3
-        a  = b;
-        b = c;
-        printf("%d\n", c);
+    printf("\n");
+    if (n == 1)
+    {
+        printf("%d\n", a);
     }
-
+    else if (n == 2)
+    {
+        printf("%d\n", a);
+        printf("%d\n", b);
+    }
+    else
+    {
+        printf("%d\n", a);
+        printf("%d\n", b);
+        for (int i = 0; i < n - 2; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            printf("%d\n", c);
+        }
+    }
     return 0;
 }
